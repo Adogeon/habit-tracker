39da63567144875369500b83472b3fef7a77e8b8
Anh Dao <addao823@gmail.com>
2020-04-26 11:52:56 -0700

    hooking up signin with firebase

--- src/page/SignIn/index.js
+++ src/page/SignIn/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useFirebase } from "react-redux-firebase";
 
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
@@ -18,31 +20,61 @@ const SignIn = (props) => {
     });
   };
 
+  const firebase = useFirebase();
+  const logInUser = (email, password) => {
+    console.log(email);
+    console.log(password);
+    firebase.login({ email, password });
+  };
+
+  const history = useHistory();
+  const handleButtonClick = (event) => {
+    const { name } = event.currentTarget;
+    if (name === "complete") {
+      logInUser(user.email, user.password);
+      history.push("/");
+    } else if (name === "cancel") {
+      history.goBack();
+    }
+  };
+
   return (
     <main>
       <Typography variant="h2">Sign In</Typography>
-      <Grid>
-        <form>
+      <form noautofill>
+        <Grid container direction="column" spacing={1}>
           <TextField
             variant="outlined"
-            label="Username"
-            name="username"
+            label="Email"
+            name="email"
             onChange={handleOnChange}
           />
           <TextField
-            variant="outline"
+            variant="outlined"
             label="Password"
             name="password"
             onChange={handleOnChange}
           />
-          <Button variant="contained" color="primary">
-            Done
-          </Button>
-          <Button variant="contained" color="secondary">
-            Cancel
-          </Button>
-        </form>
-      </Grid>
+          <Grid item container direction="row" justify="space-around">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleButtonClick}
+              name="complete"
+            >
+              Done
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              name="cancel"
+              onClick={handleButtonClick}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
     </main>
   );
 };
