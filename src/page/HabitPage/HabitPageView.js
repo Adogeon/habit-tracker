be3d16d6535fbcbb9e683cf4e8bfde8176e56e22
Anh Dao <addao823@gmail.com>
2020-05-06 09:57:48 -0700

    Update HabitPageView: chart data is linked with doneDateArray from database record

--- src/page/HabitPage/HabitPageView.js
+++ src/page/HabitPage/HabitPageView.js
@@ -28,172 +28,148 @@ import moment from "moment";
 //TODO: --DONE create a mock dataset
 //TODO: --DONE adding the chart to see fit
 
-const data = [
-  {
-    date: "2020-04-21",
-    count: 1,
-  },
-  {
-    date: "2020-10-30",
-    count: 1,
-  },
-  {
-    date: "2020-04-18",
-    count: 1,
-  },
-  {
-    date: "2020-03-08",
-    count: 1,
-  },
-  {
-    date: "2020-01-08",
-    count: 1,
-  },
-  {
-    date: "2020-11-25",
-    count: 1,
-  },
-  {
-    date: "2020-01-23",
-    count: 1,
-  },
-];
+const createDataArr = (dateArr) => {
+  return dateArr.map((date) => ({ date: date, count: 1 }));
+};
 
-const HabitPage = ({ habit }) => (
-  <Grid container direction="column" xs={12}>
-    <Grid container justify="space-between">
-      <Typography variant="h4">{habit.name}</Typography>
-      <IconButton>
-        <EditIcon fontSize="small" />
-      </IconButton>
+const HabitPage = ({ habit }) => {
+
+  return (
+    <Grid container direction="column" xs={12}>
+      <Grid container justify="space-between">
+        <Typography variant="h4">{habit.name}</Typography>
+        <IconButton>
+          <EditIcon fontSize="small" />
+        </IconButton>
+      </Grid>
+      <List>
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Grid container justify="space-between">
+                <Typography variant="h6">Description</Typography>
+                <IconButton>
+                  <EditIcon fontSize="small" />
+                </IconButton>
+              </Grid>
+            }
+            secondary={
+              <>
+                <Typography componet="span" variant="body2" color="textPrimary">
+                  {habit.desc}
+                </Typography>
+              </>
+            }
+          />
+        </ListItem>
+        <Divider component="li" justify="space-between" />
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Grid container justify="space-between">
+                <Typography variant="h6">Reason</Typography>
+                <IconButton>
+                  <EditIcon fontSize="small" />
+                </IconButton>
+              </Grid>
+            }
+            secondary={
+              <>
+                <Typography componet="span" variant="body2" color="textPrimary">
+                  {habit.reason}
+                </Typography>
+              </>
+            }
+          />
+        </ListItem>
+        <Divider component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary=""
+            secondary={
+              <Grid>
+                <CalendarHeatmap
+                  style={{ width: "100%", height: "auto" }}
+                  startDate={moment()
+                    .subtract("180", "days")
+                    .format("YYYY-MM-DD")}
+                  endDate={moment().format("YYYY-MM-DD")}
+                  values={createDataArr(habit.doneDateArr)}
+                  classForValue={(count) => {
+                    if (!count) {
+                      return "color-empty";
+                    }
+                    return `color-github-3`;
+                  }}
+                  showWeekdayLabels={true}
+                />
+              </Grid>
+            }
+          />
+        </ListItem>
+        <Divider component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Grid container justify="start">
+                <Typography variant="h6">Detail</Typography>
+              </Grid>
+            }
+            secondary={
+              <Table>
+                <TableBody>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      Start Date:
+                    </TableCell>
+                    <TableCell align="right">Date of starting</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      Longest Streak:
+                    </TableCell>
+                    <TableCell align="right">Longest Streak Number</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Grid container justify="start">
+                <Typography variant="h6">Reset</Typography>
+              </Grid>
+            }
+            secondary={
+              <Grid container justify="center">
+                <Button variant="contained" color="primary">
+                  RESET
+                </Button>
+              </Grid>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Grid container justify="start">
+                <Typography variant="h6">Delete</Typography>
+              </Grid>
+            }
+            secondary={
+              <Grid container justify="center">
+                <Button variant="contained" color="secondary">
+                  DELETE
+                </Button>
+              </Grid>
+            }
+          />
+        </ListItem>
+      </List>
     </Grid>
-    <List>
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={
-            <Grid container justify="space-between">
-              <Typography variant="h6">Description</Typography>
-              <IconButton>
-                <EditIcon fontSize="small" />
-              </IconButton>
-            </Grid>
-          }
-          secondary={
-            <>
-              <Typography componet="span" variant="body2" color="textPrimary">
-                {habit.desc}
-              </Typography>
-            </>
-          }
-        />
-      </ListItem>
-      <Divider component="li" justify="space-between" />
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={
-            <Grid container justify="space-between">
-              <Typography variant="h6">Reason</Typography>
-              <IconButton>
-                <EditIcon fontSize="small" />
-              </IconButton>
-            </Grid>
-          }
-          secondary={
-            <>
-              <Typography componet="span" variant="body2" color="textPrimary">
-                {habit.reason}
-              </Typography>
-            </>
-          }
-        />
-      </ListItem>
-      <Divider component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary=""
-          secondary={
-            <Grid>
-              <CalendarHeatmap
-                style={{ width: "100%", height: "auto" }}
-                startDate={moment()
-                  .subtract("180", "days")
-                  .format("YYYY-MM-DD")}
-                endDate={moment().format("YYYY-MM-DD")}
-                values={data}
-                classForValue={(count) => {
-                  if (!count) {
-                    return "color-empty";
-                  }
-                  return `color-github-3`;
-                }}
-                showWeekdayLabels={true}
-              />
-            </Grid>
-          }
-        />
-      </ListItem>
-      <Divider component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={
-            <Grid container justify="start">
-              <Typography variant="h6">Detail</Typography>
-            </Grid>
-          }
-          secondary={
-            <Table>
-              <TableBody>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    Start Date:
-                  </TableCell>
-                  <TableCell align="right">Date of starting</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    Longest Streak:
-                  </TableCell>
-                  <TableCell align="right">Longest Streak Number</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          }
-        />
-      </ListItem>
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={
-            <Grid container justify="start">
-              <Typography variant="h6">Reset</Typography>
-            </Grid>
-          }
-          secondary={
-            <Grid container justify="center">
-              <Button variant="contained" color="primary">
-                RESET
-              </Button>
-            </Grid>
-          }
-        />
-      </ListItem>
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={
-            <Grid container justify="start">
-              <Typography variant="h6">Delete</Typography>
-            </Grid>
-          }
-          secondary={
-            <Grid container justify="center">
-              <Button variant="contained" color="secondary">
-                DELETE
-              </Button>
-            </Grid>
-          }
-        />
-      </ListItem>
-    </List>
-  </Grid>
-);
+  );
+};
 
 export default HabitPage;
